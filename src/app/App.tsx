1a85b013fb4d8a188c1b49a76521c95b26186851
shinkar94 <romka94shinkarenko@gmail.com>
2023-07-05 20:08:07 +0300

    refactor: Add background in InfoTarot.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,16 @@ import {Header} from "../features/Header/Header";
 import {ThemeProvider} from "styled-components";
 import Theme from './ThemeData'
 import {InfoTarot} from "../features/InfoTarot/InfoTarot";
+import {TransitionLine} from "../common/TransitionLine";
+import {Programme} from "../features/Programm/Programme";
 function App() {
   return (
     <div className="App">
         <ThemeProvider theme={Theme}>
             <Header />
             <InfoTarot />
+            <TransitionLine nameLine={'Программа'}/>
+            <Programme />
         </ThemeProvider>
     </div>
   );
--- src/features/InfoTarot/InfoTarot.tsx
+++ src/features/InfoTarot/InfoTarot.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from "styled-components";
-import AuthorImg from '../../img/Avtor.png'
+import AuthorImg from '../../img/Author.png'
+import BackImg from '../../img/topim.jpg'
 
 export const InfoTarot = () => {
     return (
         <InfoBlock>
             <div className="log_img"><img src={AuthorImg} alt="AuthorImg"/></div>
             <div className="header-text">
-                <div className="shadow_hed"></div>
-                <div className="back_img"></div>
+                <div className="back_img">
+                    <div className={"img_back"}></div>
+                    <div className={"img_shadow"}></div>
+                </div>
                 <h1 className="header-h1">Пробуждение</h1>
                 <p className="header-p">Серия мероприятий, направленных на раскрытие Вашего потенциала.Объединение людей
                     со схожей потребностью для достижения желаемых результатов.</p>
@@ -24,5 +27,33 @@ export const InfoTarot = () => {
 };
 
 const InfoBlock = styled.div`
+  position: relative;
   display: flex;
+
+  .back_img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100vw;
+    z-index: -1;
+    border-radius: 10vw 50vw 3vw 50vw;
+    overflow: hidden;
+    .img_back {
+      width: 100%;
+      height: 100%;
+      background-image: url(${BackImg});
+      background-size: cover;
+      background-position: center center;
+    }
+
+    .img_shadow {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100%;
+      background: rgba(229, 214, 183, 0.63);
+    }
+  }
 `

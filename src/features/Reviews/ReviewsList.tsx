2d1ce1bb3ed6ae385d0a8b01341b83251ad1f532
shinkar94 <romka94shinkarenko@gmail.com>
2023-09-03 21:33:43 +0300

    style: add style in Reviews page

--- src/features/Reviews/ReviewsList.tsx
+++ src/features/Reviews/ReviewsList.tsx
@@ -7,24 +7,42 @@ export const ReviewsList = () => {
     const {reviews_data} = Reviews
     return (
         <StReviewsList>
-            {reviews_data.map(rev => {
-                return(
-                    <div className={'list_block'} key={rev.id}>
-                        <img src={userIcon} alt="userIcon"/>
-                        <h4>{rev.name}</h4>
-                        <p>{rev.message}</p>
-                    </div>
-                )
-            })}
+            <div className={'carusel'}>
+                {reviews_data.map(rev => {
+                    return(
+                        <div className={'list_block'} key={rev.id}>
+                            <img src={userIcon} alt="userIcon"/>
+                            <h4>{rev.name}</h4>
+                            <p>{rev.message}</p>
+                        </div>
+                    )
+                })}
+            </div>
+
         </StReviewsList>
     );
 };
 
 const StReviewsList = styled.div`
-  & img{
-    width: 4vw;
+  display: flex;
+  .carusel{
+    display: flex;
+    width: 300px;
+    background: #B34D36;
+    overflow: hidden;
+    .list_block{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      min-width: 280px;
+      padding-left: 10px;
+    }
+    & img{
+      width: 4vw;
+    }
+    & h4, p{
+      margin: 2px;
+    }
   }
-  & h4, p{
-    margin: 2px;
-  }
+  
 `

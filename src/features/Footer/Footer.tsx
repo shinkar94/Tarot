d6451793530439f4fdbd7087acd8aa4c1dfa62a9
shinkar94 <romka94shinkarenko@gmail.com>
2023-09-06 21:10:05 +0300

    feat: added helper and update style in footer

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import {useWindowSize} from "../../assets/helper/useWindowSize";
 
 export const Footer = () => {
+    const size = useWindowSize()
     return (
-        <StFooter>
-            <div className={'Contact'}>
+        <StFooter $size={size}>
+            <div className={'Contact block-info'}>
                 <p>Почта: <span>milatarot21@gmail.com</span></p>
                 <p>Инстаграм: <span>mila</span></p>
                 <p>Телеграм: <span>mila</span></p>
             </div>
-            <div className={'IP'}>
+            <div className={'IP block-info'}>
                 <p>Самозанятый</p>
                 <p>Тарот Мила Викторовна</p>
                 <p>ИНН 34661201297912</p>
             </div>
-            <div className={'docs'}>
+            <div className={'docs block-info'}>
                 <p><span>Политика конедициальности</span></p>
                 <p><span>Договор оферты</span></p>
             </div>
@@ -22,10 +24,17 @@ export const Footer = () => {
     );
 };
 
-const StFooter = styled.div`
+const StFooter = styled.div<{$size: number}>`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-around;
   box-shadow: 0 -5px 5px #6c6c6c;
+  .block-info{
+    ${(props) => props.$size < 1000 && css`
+      text-align: center;
+      width: 100%;
+    `}
+  }
   & span{
     color: #B34D36;
     font-size: 24px;
